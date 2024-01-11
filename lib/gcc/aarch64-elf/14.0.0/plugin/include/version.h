9be008068f35d26d80546a12bfb16d2999fb9464
dexternoob <noobbeastdext@gmail.com>
2024-01-11 09:15:49 +0530

    Import ARM64 GCC 20240111
    Build completed on: Thu Jan 11 09:15:49 IST 2024
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/__w/gcc-build/gcc-arm64/bin/../libexec/gcc/aarch64-elf/14.0.0/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c,c++,fortran --enable-threads=posix --prefix=/__w/gcc-build/gcc-build/../gcc-arm64 --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Noob GCC' --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 14.0.0 20240111 (Dexter) (Noob GCC)
    
    LLD: LLD 18.0.0 (compatible with GNU linkers)
    Signed-off-by: dexternoob <noobbeastdext@gmail.com>

--- lib/gcc/aarch64-elf/14.0.0/plugin/include/version.h
+++ lib/gcc/aarch64-elf/14.0.0/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "14.0.0 20240107 (Dexter)"
+#define version_string "14.0.0 20240111 (Dexter)"
 #define pkgversion_string "(Noob GCC) "
 
 /* This is the location of the online document giving instructions for
